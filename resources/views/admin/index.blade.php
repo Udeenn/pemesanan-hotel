b8e18a291f20fce8e3243ea301091768b1831b0f
Ammar Miftahudin Anshori <ammarmiftah.06@gmail.com>
2024-12-12 04:51:03 +0700

    add page admin create room

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,47 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container mx-4">
+        <h2>Daftar Ruangan</h2>
+        <ul>
+            <a class="btn btn-success" href="{{ route('admin.create') }}">Tambah Ruangan</a>
+        </ul>
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nama Ruangan</th>
+                    <th>Deskripsi</th>
+                    <th>Harga</th>
+                    <th>Gambar</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($rooms as $room)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $room->room_name }}</td>
+                        <td>{{ $room->room_description }}</td>
+                        <td>Rp {{ number_format($room->room_price, 0, ',', '.') }}</td>
+                        <td><img src="{{ asset('images/' . $room->image) }}" alt="room Image" width="50"></td>
+                        <td>
+                            <a href="{{ route('admin.edit', $room->id_room) }}" class="btn btn-warning">Edit</a>
+                            <form action="{{ route('admin.destroy', $room->id_room) }}" method="POST" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger" onclick="return confirm('Apakah Anda yakin ingin menghapus produk ini?')">Hapus</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MRoomController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,14 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', function () {
-    return view('admin.index');
-})->middleware(['auth', 'verified'])->name('admin');
+Route::get('/admin', [MRoomController::class, 'index'])->middleware(['auth', 'verified'])->name('admin');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/admin/room/create', [MRoomController::class, 'create'])->name('admin.create');
+    Route::post('/admin/room', [MRoomController::class, 'store'])->name('admin.store');
+    Route::get('/admin/room/{id}/edit', [MRoomController::class, 'edit'])->name('admin.edit');
+    Route::put('/admin/room/{id}', [MRoomController::class, 'update'])->name('admin.update');
+    Route::delete('/admin/room/{id}', [MRoomController::class, 'destroy'])->name('admin.destroy');
 });
 
 require __DIR__.'/auth.php';
